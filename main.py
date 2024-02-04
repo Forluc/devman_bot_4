71bc07f925526b6b273e84366401511e20598065
Dmitrii Masliaev <m6ytcbi@gmail.com>
2024-02-04 18:57:04 +0400

    parse quiz

--- main.py
+++ main.py
@@ -1,11 +1,23 @@
 import os
 
 
+def parse_quiz(file):
+    quiz = {}
+    for text in file.read().split("\n\n"):
+        if not text:
+            continue
+        header, body = text.split("\n", maxsplit=1)
+        if header.startswith("Вопрос"):
+            question = " ".join(body.split())
+        if header.startswith("Ответ"):
+            quiz[question] = " ".join(body.split())
+    return quiz
+
+
 def main():
-    questions = os.path.join('quiz-question', '1vs1200.txt')
+    questions = os.path.join('quiz-question', '3f15.txt')
     with open(questions, encoding='KOI8-R') as file:
-        questions = file.read()
-    print(questions)
+        quiz = parse_quiz(file)
 
 
 if __name__ == '__main__':
